29d674ce22fe25640b635c9f49046e3d2b0c319e
Akshat Sachdeva <92359403+Akshatsachdev@users.noreply.github.com>
2022-05-03 20:55:22 +0530

    Add files via upload

---
+++ calander.py
@@ -0,0 +1,34 @@
+import calendar
+#import tkinter module
+from tkinter import *
+#This function displays calendar for a given year
+def showCalender():
+    gui = Tk()
+    gui.config(background='black')
+    gui.title("Calender for the year")
+    gui.geometry("550x600")
+    year = int(year_field.get())
+    gui_content= calendar.calendar(year)
+    calYear = Label(gui, text= gui_content, font= "Consolas 10 bold")
+    calYear.grid(row=5, column=1,padx=20)
+    gui.mainloop()
+#Driver code
+if __name__=='__main__':
+    new = Tk()
+    new.config(background='orange')
+    new.title("Calender")
+    new.geometry("2500x1400")
+    cal = Label(new, text="Calender",bg='grey',font=("times", 28, "bold"))
+    #Label for enter year
+    year = Label(new, text="Enter year", bg='dark grey')
+    #text box for year input
+    year_field=Entry(new)
+    button = Button(new, text='Show Calender',fg='Black',bg='Blue',command=showCalender)
+    Exit = Button(new, text='exit',fg='Black',bg='Blue')
+    #adjusting widgets in position
+    cal.grid(row=1, column=1)
+    year.grid(row=2, column=1)
+    year_field.grid(row=3, column=1)
+    button.grid(row=4, column=1)
+    Exit.grid(row=6, column=1)
+    new.mainloop()
