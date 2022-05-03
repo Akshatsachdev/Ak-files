d1ad27bc57ffc0e6b75f405a8a97afda8f634a0e
Akshat Sachdeva <92359403+Akshatsachdev@users.noreply.github.com>
2022-05-03 21:03:14 +0530

    Add files via upload

---
+++ Word game (2).py
@@ -0,0 +1,212 @@
+print("WORD GAME".center(80," "))
+user=input("Enter your name:")
+name=user
+print("Welcome"+" " + name)
+for i in range(1,4):
+    print(" ")
+
+for i in range(1,4):
+    print(" ")
+print("You have 15 chances to guess the word")
+
+
+
+
+
+
+print("Level 1".center(90," " ))
+for k in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A palce with facilities for improving physical fitness'")
+for j in range(1,16):
+    word=input("Enter a word:")
+    word1=(word.upper())
+    if word1==("GYM"):
+        print("YOU ARE PASS")
+        break
+    else:
+        print("No")
+print("The word is GYM")
+
+
+
+
+
+print("Level 2".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'An exciting and daring experience:'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("ADVENTURE"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is ADVENTURE")
+
+
+
+
+
+
+print("Level 3".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A pale yellow citrus fruit with acidic juice'")
+for m in range(1,16):
+    word=input("Enter a word")
+    word1=word.upper()
+    if word1==("LEMON"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is Lemon")
+
+
+
+
+
+print("Level 4".center(90," " ))
+for l in range(1,3):
+  print(" ")
+print("All the best")
+print("The hint is 'A car safety device that is fill with air in a collision to protect the driver'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("AIRBAG"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is AIRBAG")        
+
+
+
+
+
+
+print("Level 5".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'The surrounding in wich a person, animal or plant lives'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("ENVIRONMENT"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is Environment")
+
+
+
+
+
+
+print("Level 6".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A system of recording and reproducing moving image'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("VIDEO"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is video")
+
+
+
+
+
+
+print("Level 7".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A person who report's news'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("REPORTER"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is reporter")
+
+
+
+
+
+print("Level 8".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A device intended to explode and cause damage'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("BOMB"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is Bomb")
+
+
+
+
+
+print("Level 9".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'An instrument that show's time'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("CLOCK"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is clock")
+
+
+
+
+
+print("Level 10".center(90," " ))
+for l in range(1,3):
+    print(" ")
+print("All the best")
+print("The hint is 'A part of a plant from which the seed or fruit develops'")
+for m in range(1,16):
+    word=input("Enter a word:")
+    word1=word.upper()
+    if word1==("FLOWER"):
+        print("You are pass")
+        break
+    else:
+        print("No")
+print("The word is FLOWER")
+print("Type yes for next question")
+user2=input("Do you finish your game:")
+user3=user2.upper()
+if user3=="YES":
+    print("Thank you")
+print("Thank you".center(90," "))
+
